f6f36a36982d73e77996e920a9d4a03fa5e08060
PyLukasz <as.lukasz@gmail.com>
2022-06-07 09:56:10 +0200

    Added text color change to changed lines. Added functions to find lines with tool change and added preparation of the tool.

--- GCodeModifier/Program.cs
+++ GCodeModifier/Program.cs
@@ -8,6 +8,7 @@ namespace GCodeModifier
     {
         static void Main(string[] args)
         {
+            string text = "Lukasz";
             string textFile = @"D:\P1.txt";
             string[] lines = File.ReadAllLines(textFile);
             var numberOfTLines = new List<int>();
@@ -18,25 +19,28 @@ namespace GCodeModifier
                 if (lines[i].Contains('T'))
                 {
                     numberOfTLines.Add(i);
-                    listOfTLines.Add(lines[i].Substring(0, 2));
+                    int lenghtOfLine = lines[i].Length - 1;
+                    int indexOfFirstSpacebar = lines[i].IndexOf(' ');
+                    string removeFirstSpacebar = lines[i].Substring(indexOfFirstSpacebar + 1);
+                    int indexOfSecondSpacebar = removeFirstSpacebar.IndexOf(' ');
+                    int indexOfT = lines[i].IndexOf('T');
+                    listOfTLines.Add(lines[i].Substring(indexOfT, indexOfSecondSpacebar));
                 }
             }
-            foreach (var item in numberOfTLines)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-
-            int count = 0;
+  
+            int count = 1;
             foreach (var item in lines)
             {
+                if (count == numberOfTLines.Count)
+                {
+                    count = 0;
+                }
                 if (item.Contains('T'))
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine(item + "\n" + listOfTLines[count]);
+                    Console.ForegroundColor = ConsoleColor.White;
                     count += 1;
-
                 }
                 else
                 {
