7a0bd834392e27da411e95302c97c5f30ece7083
PyLukasz <as.lukasz@gmail.com>
2022-06-08 08:42:04 +0200

    Refactor, moved code from main class to added new  class

--- GCodeModifier/GCodeModify.cs
+++ GCodeModifier/GCodeModify.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,61 @@ namespace GCodeModifier
 {
     public class GCodeModify
     {
+        public string[] ReadFile(string path)
+        {
+            string[] readFile = File.ReadAllLines(path);
+            return readFile;
+        }
 
+        public string[] ModifyFile(string[] file)
+        {
+            List<string> newFile = new List<string>();
+            var numberOfTLines = new List<int>();
+            var listOfTLines = new List<string>();
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (file[i].Contains('T'))
+                {
+                    numberOfTLines.Add(i);
+                    int lenghtOfLine = file[i].Length - 1;
+                    int indexOfFirstSpacebar = file[i].IndexOf(' ');
+                    string removeFirstSpacebar = file[i].Substring(indexOfFirstSpacebar + 1);
+                    int indexOfSecondSpacebar = removeFirstSpacebar.IndexOf(' ');
+                    int indexOfT = file[i].IndexOf('T');
+                    listOfTLines.Add(file[i].Substring(indexOfT, indexOfSecondSpacebar));
+                }
+            }
+            int count = 1;
+            foreach (var item in file)
+            {
+                if (count == numberOfTLines.Count)
+                {
+                    count = 0;
+                }
+                if (item.Contains('T'))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    newFile.Add(item);
+
+                    newFile.Add(listOfTLines[count]);
+
+                    Console.WriteLine(item + "\n" + listOfTLines[count]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    count += 1;
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                    newFile.Add(item);
+
+                }
+
+            }
+
+
+
+            return newFile.ToArray();
+        }
     }
 }
--- GCodeModifier/Program.cs
+++ GCodeModifier/Program.cs
@@ -12,52 +12,12 @@ namespace GCodeModifier
             string textFile = Console.ReadLine();
             Console.Write("Get changed file path: ");
             string changedFile = Console.ReadLine();
-            string[] lines = File.ReadAllLines(textFile);
-            List<string> newFile = new List<string>();
-            var numberOfTLines = new List<int>();
-            var listOfTLines = new List<string>();
 
-            for (int i = 0; i < lines.Length; i++)
-            {
-                if (lines[i].Contains('T'))
-                {
-                    numberOfTLines.Add(i);
-                    int lenghtOfLine = lines[i].Length - 1;
-                    int indexOfFirstSpacebar = lines[i].IndexOf(' ');
-                    string removeFirstSpacebar = lines[i].Substring(indexOfFirstSpacebar + 1);
-                    int indexOfSecondSpacebar = removeFirstSpacebar.IndexOf(' ');
-                    int indexOfT = lines[i].IndexOf('T');
-                    listOfTLines.Add(lines[i].Substring(indexOfT, indexOfSecondSpacebar));
-                }
-            }
-            int count = 1;
-            foreach (var item in lines)
-            {
-                if (count == numberOfTLines.Count)
-                {
-                    count = 0;
-                }
-                if (item.Contains('T'))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    newFile.Add(item);
+            GCodeModify gCodeModify = new GCodeModify();
+            var file = gCodeModify.ReadFile(textFile);
+            var modify = gCodeModify.ModifyFile(file);
 
-                    newFile.Add(listOfTLines[count]);
-
-                    Console.WriteLine(item + "\n" + listOfTLines[count]);
-                    Console.ForegroundColor = ConsoleColor.White;
-                    count += 1;
-                }
-                else
-                {
-                    Console.WriteLine(item);
-                    newFile.Add(item);
-
-                }
-
-            }
-
-            File.WriteAllLines(changedFile, newFile.ToArray());
+            File.WriteAllLines(changedFile, modify);
         }
     }
 }
